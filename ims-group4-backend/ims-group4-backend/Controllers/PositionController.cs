f4edb5b3a319f3716ecf943062470abb5892bf56
joelAndreasson <joelaron007@gmail.com>
2023-04-06 16:48:37 +0200

    Added PositionController

---
+++ ims-group4-backend/ims-group4-backend/Controllers/PositionController.cs
@@ -0,0 +1,13 @@
+
+using ims_group4_backend.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ims_group4_backend.Controllers{
+    [ApiController]
+    [Route("api/position")]
+    public class ApiController : ControllerBase{
+        
+    }
+}
